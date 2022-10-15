8c226a0c480bbfb5e01bdd2330d385e8a190eca0
Zlatko Pračić <53391918+bujin2502@users.noreply.github.com>
2022-10-15 12:53:45 +0200

    Update 16145097_vj1.cpp

--- 16145097_vj1.cpp
+++ 16145097_vj1.cpp
@@ -5,77 +5,68 @@
 
 using namespace std;
 
-void(*sigset(int sig, void (*disp)(int)))(int);
-
 int N;
 int n;
 
 void prekid(int sig)
 {
-    if (SIGTSTP)
-        cout << "Izlazak iz igre!" << endl;
+    cout << "Izlazak iz igre!" << endl;
     exit(0);
 }
 
-void odabir(int sig)
+void odabir_A(int sig)
 {
-    switch (sig)
+    sighold(SIGINT);
+    sighold(SIGQUIT);
+    cout << "Zigica igrac A uzeo sa stola:" << endl;
+    do
     {
-    case SIGINT:
-    {
-        sighold(sig);
-        cout << "Zigica igrac A uzeo sa stola:\n";
-        do
+        cin >> n;
+        if (n < 1 || n > 3 || n > N)
         {
-            cin >> n;
-            if (n < 1 || n > 3 || n > N)
-            {
-                cout << "Broj mora biti izmedu 1 i 3 i ne moze biti veci od broja zigica na stolu!\n";
-            }
-        } while (n < 1 || n > 3 || n > N);
-        N = N - n;
-        sigrelse(sig);
-        break;
-    }
-    case SIGQUIT:
+            cout << "Broj mora biti izmedu 1 i 3 i ne moze biti veci od broja zigica na stolu!" << endl;
+        }
+    } while (n < 1 || n > 3 || n > N);
+    N = N - n;
+}
+
+void odabir_B(int sig)
+{
+    sighold(SIGINT);
+    sighold(SIGQUIT);
+    cout << "Zigica igrac B uzeo sa stola:" << endl;
+    do
     {
-        sighold(sig);
-        cout << "Zigica igrac B uzeo sa stola:\n";
-        do
+        cin >> n;
+        if (n < 1 || n > 3 || n > N)
         {
-            cin >> n;
-            if (n < 1 || n > 3 || n > N)
-            {
-                cout << "Broj mora biti izmedu 1 i 3 i ne moze biti veci od broja zigica na stolu!\n";
-            }
-        } while (n < 1 || n > 3 || n > N);
-        N = N - n;
-        sigrelse(sig);
-        break;
-    }
-    }
+            cout << "Broj mora biti izmedu 1 i 3 i ne moze biti veci od broja zigica na stolu!" << endl;
+        }
+    } while (n < 1 || n > 3 || n > N);
+    N = N - n;
 }
 
 int main(int argc, char *argv[])
 {
 
+    pause();
     if (argc != 2)
     {
-        cout << "Ovaj program mora primiti jedan argument!\n";
-        cout << "Pri unosu programa u komandnoj liniji na kraju dodajte jedan broj veci od broj 3.\n";
+        cout << "Ovaj program mora primiti jedan argument!" << endl;
+        cout << "Pri unosu programa u komandnoj liniji na kraju dodajte jedan broj veci od broj 3." << endl;
         return 0;
     }
     N = atoi(argv[1]);
 
     if (N < 4)
     {
-        cout << "Unesen je broj manji od broja 4! Morate unjeti broj veci od broja 3.\n";
+        cout << "Unesen je broj manji od broja 4! Morate unjeti broj veci od broja 3." << endl;
         return 0;
     }
 
-    cout << "Parametri su ispravno uneseni.\n";
-    cout << "Unesen je broj zigica i isti iznosi: " << N << ".\n";
-    cout << "Tijekom igre mozete povlaciti jednu, dvije ili tri zigice.\n";
+    cout << "Parametri su ispravno uneseni." << endl;
+    cout << "Unesen je broj zigica i isti iznosi: " << N << "." << endl;
+    cout << "Tijekom igre mozete povlaciti jednu, dvije ili tri zigice." << endl;
 
     sigset(SIGTSTP, prekid);
 
@@ -85,14 +76,16 @@ int main(int argc, char *argv[])
     {
         if (igrac_a)
         {
-            cout << "Zigica na stolu: " << N << ". Na redu igrac A. Unesi Ctrl-C.\n";
-            sigset(SIGINT, odabir);
+            sighold(SIGQUIT);
+            cout << "Zigica na stolu: " << N << ". Na redu igrac A. Unesi Ctrl-C." << endl;
+            sigset(SIGINT, odabir_A);
             pause();
         }
         else
         {
-            cout << "Zigica na stolu: " << N << ". Na redu igrac B. Unesi Ctrl-\\\n";
-            sigset(SIGQUIT, odabir);
+            sighold(SIGINT);
+            cout << "Zigica na stolu: " << N << ". Na redu igrac B. Unesi Ctrl-\\" << endl;
+            sigset(SIGQUIT, odabir_B);
             pause();
         }
         igrac_a = !igrac_a;
@@ -100,14 +93,14 @@ int main(int argc, char *argv[])
 
     if (N < 1)
     {
-        cout << "Ostalo je " << N << " zigica na stolu.\n";
+        cout << "Ostalo je " << N << " zigica na stolu." << endl;
         if (igrac_a)
         {
-            cout << "Pobjednik je igrac A! Cestitam!\n";
+            cout << "Pobjednik je igrac A! Cestitam!" << endl;
         }
         else
         {
-            cout << "Pobjednik je igrac B! Cestitam!\n";
+            cout << "Pobjednik je igrac B! Cestitam!" << endl;
         }
     }
     return 0;
