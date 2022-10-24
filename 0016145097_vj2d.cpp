92e07de6caebc66f97fd7f57444d40ac116d3455
Zlatko Pračić <53391918+bujin2502@users.noreply.github.com>
2022-10-24 14:13:07 +0200

    Update 0016145097_vj2d.cpp

--- 0016145097_vj2d.cpp
+++ 0016145097_vj2d.cpp
@@ -5,6 +5,10 @@
 #include <ctime>
 #include <sys/types.h>
 #include <unistd.h>
+#include <wait.h>
+#include <sys/ipc.h>
+#include <sys/shm.h>
+#include <csignal>
 
 using namespace std;
 
@@ -13,6 +17,32 @@ struct slog_info_polje {
     int br_el;
     int blok;
 } *info_polje;
+
+void proces (int i) {
+    int start = info_polje->blok * i;
+    int kraj = start + info_polje->blok;
+
+    if (kraj > info_polje->br_el) {
+        kraj = info_polje->br_el;
+    }
+
+    printf("Dijete %d\n", i);
+
+    for (int i = start; i < kraj; i++) {
+            long double e = exp(info_polje->polje[i]);
+            printf("%Lf\n", e);
+    }
+}
+
+key_t id_dij_mem;
+
+int broj_procesa;
+void kraj (int sig) {
+    for (int i = 0; i < broj_procesa; i++) wait(NULL);
+    shmdt(info_polje);
+    shmctl(id_dij_mem, IPC_RMID, NULL);
+    exit(0);
+}
 
 int main (int argc, char **argv) {
     system("clear");
@@ -23,10 +53,14 @@ int main (int argc, char **argv) {
         return 0;
     }
 
-    info_polje = new slog_info_polje;
+    id_dij_mem = shmget(IPC_PRIVATE, sizeof(slog_info_polje), 0600);
+
+    info_polje = (slog_info_polje *) shmat(id_dij_mem, NULL, 0);
     info_polje->br_el = atoi(argv[1]);
     info_polje->blok = atoi(argv[2]);
     info_polje->polje = new long double [info_polje->br_el];
+
+    sigset(SIGINT, kraj);
 
     srand(time(0));
     for (int i = 0; i < info_polje->br_el; i++)
@@ -35,24 +69,25 @@ int main (int argc, char **argv) {
         cout << i << ". " << setprecision(12) << info_polje->polje[i] << endl;
     }
 
-    int broj_procesa = info_polje->br_el / info_polje->blok;
+    broj_procesa = info_polje->br_el / info_polje->blok;
     if (info_polje->br_el % info_polje->blok) broj_procesa++;
-/*
+
     for (int i = 0; i < broj_procesa; i++)
     {
         switch (fork())
         {
         case 0:
         {
-
+            proces(i);
+            exit(0);
         }
         case -1:
         {
-
+            cout << "Fatalna pogreška!\n";
+            exit(1);
         }
         }
     }
-*/
-    delete info_polje;
-    return 0;
+
+    kraj(0);
 }
