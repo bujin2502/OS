f8250eb08ac19bb33138366d4e053f522d2fde50
Zlatko Pračić <53391918+bujin2502@users.noreply.github.com>
2022-12-04 17:16:59 +0100

    Update 0016145097_vj4a.cpp

--- 0016145097_vj4a.cpp
+++ 0016145097_vj4a.cpp
@@ -6,24 +6,41 @@
 using namespace std;
 
 sem_t semafor;
+pthread_mutex_t mutex;
 pthread_t *polje_dretvi;
+pthread_cond_t uvijet;
 int *polje_i;
 int broj_dretvi;
+int broj_unesenih = 0;
 
 void *dretva(void *z)
 {
-    sem_wait(&semafor);
-    int broj;
-    printf("Unesi broj: \n");
-    cin >> broj;
-    printf("Uneseni broj je: %d\n", broj);
+    int i = *(int *)z;
     sem_post(&semafor);
+    pthread_mutex_lock(&mutex);
+    int broj;
+    printf("Dretva broj: %d. Unesi broj: ", i + 1);
+    cin >> broj;
+    broj_unesenih++;
+    if (broj_unesenih < broj_dretvi)
+    {
+        pthread_cond_wait(&uvijet, &mutex);
+    }
+    else
+    {
+        pthread_cond_broadcast(&uvijet);
+    }
+
+    printf("Unesen broj: %d u dretvu: %d\n", broj, i + 1);
+    pthread_mutex_unlock(&mutex);
     pthread_exit(z);
 }
 
 void prekid(int sig)
 {
     sem_destroy(&semafor);
+    pthread_mutex_destroy(&mutex);
+    pthread_cond_destroy(&uvijet);
     for (int i = 0; i < broj_dretvi; i++)
         pthread_join(polje_dretvi[i], NULL);
     delete[] polje_dretvi;
@@ -42,16 +59,17 @@ int main(int argc, char **argv)
     }
 
     sem_init(&semafor, 0, 1);
+    pthread_mutex_init(&mutex, NULL);
+    pthread_cond_init(&uvijet, NULL);
     broj_dretvi = atoi(argv[1]);
     polje_dretvi = new pthread_t[broj_dretvi];
     polje_i = new int[broj_dretvi];
-
-    sigset(SIGINT, prekid);
 
     for (int i = 0; i < broj_dretvi; i++)
     {
         polje_i[i] = i;
         pthread_create(&polje_dretvi[i], NULL, dretva, &polje_i[i]);
+        sem_wait(&semafor);
     }
 
     for (int i = 0; i < broj_dretvi; i++)
@@ -59,7 +77,11 @@ int main(int argc, char **argv)
         pthread_join(polje_dretvi[i], NULL);
     }
 
+    sigset(SIGINT, prekid);
+
     sem_destroy(&semafor);
+    pthread_mutex_destroy(&mutex);
+    pthread_cond_destroy(&uvijet);
     delete[] polje_dretvi;
     delete[] polje_i;
 
