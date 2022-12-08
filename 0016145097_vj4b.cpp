10da7baf6ec10e4d565a728d00d627ce219f45f7
Zlatko Pračić <53391918+bujin2502@users.noreply.github.com>
2022-12-08 14:16:46 +0100

    Update 0016145097_vj4b.cpp

--- 0016145097_vj4b.cpp
+++ 0016145097_vj4b.cpp
@@ -6,16 +6,20 @@
 using namespace std;
 
 pthread_t *polje_dretvi_nakupac;
+int *polje_dr_nakupac;
+int broj_nakupaca = 3;
+
 pthread_t *dretva_veletrgovac;
 
 pthread_mutex_t mutex1;
 pthread_mutex_t mutex2;
+
 pthread_cond_t uvjet[3];
 pthread_cond_t uvjet_veletrgovac;
+
+bool moze;
 int komplet[2];
-int *polje_dr_nakupac;
-int broj_nakupaca = 3;
-bool kompletirano;
+int broj;
 
 void stvori_ponudu()
 {
@@ -30,22 +34,33 @@ void *veletrgovac(void *z)
 {
     while (true)
     {
+        pthread_mutex_lock(&mutex1);
+        while (moze)
+        {
+            pthread_cond_wait(&uvjet_veletrgovac, &mutex1);
+        };
         stvori_ponudu();
         if ((komplet[0] == 0 && komplet[1] == 1) || (komplet[0] == 1 && komplet[1] == 0))
         {
-            printf("tipkovnica i monitor\n");
+            broj = 0;
+            printf("Veletrgovac stavio monitor i tipkovnicu\n");
             sleep(1);
         }
         else if ((komplet[0] == 0 && komplet[1] == 2) || (komplet[0] == 2 && komplet[1] == 0))
         {
-            printf("racunalo i tipkovnica\n");
+            broj = 1;
+            printf("Veletrgovac stavio tipkovnicu i racunalo\n");
             sleep(1);
         }
         else if ((komplet[0] == 1 && komplet[1] == 2) || (komplet[0] == 2 && komplet[1] == 1))
         {
-            printf("monitor i racunalo\n");
+            broj = 2;
+            printf("Veletrgovac stavio racunalo i monitor\n");
             sleep(1);
         }
+        moze = true;
+        pthread_cond_signal(&uvjet[broj]);
+        pthread_mutex_unlock(&mutex1);
     }
     pthread_exit(z);
 }
@@ -55,22 +70,31 @@ void *nakupac(void *z)
     int i = *(int *)z;
     while (true)
     {
+        pthread_mutex_lock(&mutex2);
+        while (!moze)
+        {
+            pthread_cond_wait(&uvjet[i], &mutex2);
+        };
         switch (i)
         {
-        case 0: // NAKUPAC - TIPKOVNICA
-            printf("Nakupac s tipkovnicama uzeo monitor i racunalo\n");
-            sleep(1);
-            break;
-        case 1: // NAKUPAC - MONITOR
-            printf("Nakupac s monitorima uzeo tipkovnicu i racunalo\n");
-            sleep(1);
-            break;
-        case 2: // NAKUPAC - RACUNALO
+        case 0:
             printf("Nakupac s racunalima uzeo monitor i tipkovnicu\n");
             sleep(1);
             break;
+        case 1:
+            printf("Nakupac s monitorima uzeo tipkovnicu i racunalo\n");
+            sleep(1);
+            break;
+        case 2:
+            printf("Nakupac s tipkovnicama uzeo racunalo i monitor\n");
+            sleep(1);
+            break;
         }
-    }
+        sleep(1);
+        moze = false;
+        pthread_cond_signal(&uvjet_veletrgovac);
+        pthread_mutex_unlock(&mutex2);
+    };
     pthread_exit(z);
 }
 
